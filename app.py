9af4d002dd010be188e7d8147d76b63989b57490
= <=>
2024-07-25 13:07:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import logging
+from flask import Flask
+
+app = Flask(__name__)
+
+# Configure logging
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+
+@app.route('/')
+def hello():
+    app.logger.info('Request received')
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
